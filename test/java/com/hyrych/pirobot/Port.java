6e7c39727a30c90ead5fa5a28e845c062353f8d7
yagy0913 <yaroslavhyrych@gmail.com>
2017-05-26 16:54:55 +0300

    Change port

--- test/java/com/hyrych/pirobot/Port.java
+++ test/java/com/hyrych/pirobot/Port.java
@@ -8,76 +8,38 @@ import java.util.Scanner;
  * Created by yagy0913 on 5/25/2017.
  */
 public class Port {
-    private GpioController gpio = GpioFactory.getInstance();
-    GpioPinDigitalOutput m_1_A = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_16, "MyLED", PinState.HIGH);
-    GpioPinDigitalOutput m_1_B = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_18, "MyLED", PinState.HIGH);
-    GpioPinDigitalOutput m_2_A = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_13, "MyLED", PinState.HIGH);
-    GpioPinDigitalOutput m_2_B = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_15, "MyLED", PinState.HIGH);
-
-    public Port() throws InterruptedException {
-        m_1_A.setShutdownOptions(true, PinState.LOW);
-        m_1_B.setShutdownOptions(true, PinState.LOW);
-        m_2_A.setShutdownOptions(true, PinState.LOW);
-        m_2_B.setShutdownOptions(true, PinState.LOW);
-        Thread.sleep(2000);
-    }
-
     public static void main(String[] args) throws InterruptedException {
-        final Port motor = new Port();
+        System.out.println("--> Init pins");
+        GpioController gpio = GpioFactory.getInstance();
+        GpioPinDigitalOutput[] pins = {
+                gpio.provisionDigitalOutputPin(RaspiPin.GPIO_16, "MyLED_1", PinState.HIGH),
+                gpio.provisionDigitalOutputPin(RaspiPin.GPIO_18, "MyLED_2", PinState.HIGH),
+                gpio.provisionDigitalOutputPin(RaspiPin.GPIO_13, "MyLED_3", PinState.HIGH),
+                gpio.provisionDigitalOutputPin(RaspiPin.GPIO_15, "MyLED_4", PinState.HIGH)
+        };
 
         System.out.println("--> Init CLI");
 
         Scanner sc = new Scanner(System.in);
-        boolean isReverse = false;
         int index = 0;
 
         while (true) {
-            System.out.println("Enter direction:");
+            System.out.println("Enter port:");
             index = sc.nextInt();
-            GpioPinDigitalOutput port = motor.getPort(index);
+
+//            if (index == 9) {
+//                isReverse = true;
+//                continue;
+//            }
 
             if (index == 0) {
                 break;
             }
 
-            if (index == 9) {
-                isReverse = true;
+            if (index > pins.length) {
                 continue;
             }
 
-            if (port == null) {
-                continue;
-            }
-
-            if (isReverse) {
-                motor.setHight(port);
-            } else {
-                motor.setLow(port);
-            }
-            Thread.sleep(5000);
+            pins[index - 1].toggle();
         }
-    }
-
-    private GpioPinDigitalOutput getPort(int index) {
-        switch (index) {
-            case 1:
-                return this.m_1_A;
-            case 2:
-                return this.m_1_B;
-            case 3:
-                return this.m_2_A;
-            case 4:
-                return this.m_2_B;
-        }
-        return null;
-    }
-
-    private void setLow(GpioPinDigitalOutput port) {
-        port.low();
-    }
-
-    private void setHight(GpioPinDigitalOutput port) {
-        port.high();
-    }
-
-}
+    }}
